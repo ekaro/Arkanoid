aec1f6d542fea86b975adfa672b590e18d794a16
ekaro <marek.pekarovic@gmail.com>
2017-09-04 19:37:44 +0200

    repair small mistake

--- Engine/RectF.h
+++ Engine/RectF.h
@@ -6,7 +6,7 @@ class RectF
 public:
 	RectF() = default;
 	RectF(float left_in, float right_in, float top_in, float bottom_in);
-	RectF(const Vec2& topLeft, const Vec2& topRight);
+	RectF(const Vec2& topLeft, const Vec2& bottomRight);
 	RectF(const Vec2& topLeft, float width, float height);
 	bool IsOverlapping(const RectF& other) const;
 	static RectF FromCenter(const Vec2& center, float halfWidth, float halfHeight); //static function

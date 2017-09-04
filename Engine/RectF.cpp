8c345850a44964a73a7b1c75dbde1c6f0c15ff70
Marek Pekarovič <marek.pekarovic@gmail.com>
2017-09-04 12:08:45 +0200

    added rectf

---
+++ Engine/RectF.cpp
@@ -0,0 +1,28 @@
+#include "RectF.h"
+
+RectF::RectF(float left_in, float right_in, float top_in, float bottom_in)
+	:
+	left(left_in),
+	right(right_in),
+	top(top_in),
+	bottom(bottom_in)
+{
+}
+
+RectF::RectF(const Vec2& topLeft, const Vec2& topRight)
+	:
+	RectF(topLeft.x, topLeft.y, topRight.x, topRight.y)
+{
+}
+
+RectF::RectF(const Vec2& topLeft, float width, float height)
+	:
+	RectF(topLeft, topLeft + Vec2(width, height))
+{
+}
+
+bool RectF::IsOverlapping(const RectF& other) const
+{
+	return right > other.left && bottom > other.top &&
+		left < other.right && top < other.bottom;
+}

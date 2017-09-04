a29e76ac797a81b24823a3a5664358699bdbf91f
Marek Pekarovič <marek.pekarovic@gmail.com>
2017-09-04 16:24:43 +0200

    added wall collision (wih errors)

--- Engine/RectF.cpp
+++ Engine/RectF.cpp
@@ -26,3 +26,9 @@ bool RectF::IsOverlapping(const RectF& other) const
 	return right > other.left && bottom > other.top &&
 		left < other.right && top < other.bottom;
 }
+
+RectF RectF::FromCenter(const Vec2& center, float halfWidth, float halfHeight)
+{
+	const Vec2 half(halfWidth, halfHeight);
+	return RectF(center - half, center + half);
+}
--- Engine/RectF.h
+++ Engine/RectF.h
@@ -9,6 +9,7 @@ public:
 	RectF(const Vec2& topLeft, const Vec2& topRight);
 	RectF(const Vec2& topLeft, float width, float height);
 	bool IsOverlapping(const RectF& other) const;
+	static RectF FromCenter(const Vec2& center, float halfWidth, float halfHeight); //static function
 public:
 	float left;
 	float right;

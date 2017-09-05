0011bf2a7de9e0218939c68de3556aeb8dc7b7bf
Marek Pekarovič <marek.pekarovic@gmail.com>
2017-09-05 15:29:34 +0200

    brick padding

--- Engine/Brick.h
+++ Engine/Brick.h
@@ -13,6 +13,7 @@ public:
 	void Draw(Graphics& gfx) const;
 	bool BallCollision(Ball& ball);
 private:
+	static constexpr float padding = 1.0f;
 	RectF rect;
 	Color color;
 	bool destroyed = false;
